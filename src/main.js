fdcf3bbbea82df4689c35d1048f3464cb5f2f96c
iago1221 <prog.iago.oliveira@gmail.com>
2026-06-04 22:05:51 -0300

    mapa liso

--- src/main.js
+++ src/main.js
@@ -56,11 +56,21 @@ function initMap() {
 
   const satelite = L.layerGroup([sateliteImagery, sateliteLabels]);
 
+  const liso = L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: "abcd",
+      maxZoom: 19,
+    }
+  );
+
   ruas.addTo(map);
 
   L.control
     .layers(
-      { Ruas: ruas, Satélite: satelite },
+      { Ruas: ruas, Liso: liso, Satélite: satelite },
       null,
       { position: "topright" }
     )
